d4c65bbc9c1abfb47d689375fc945c0eaf46d447
Onur Kol <onurkol4161@gmail.com>
2017-04-20 22:51:02 +0000

    Update gethash.js

--- gethash.js
+++ gethash.js
@@ -1,7 +1,7 @@
 /*
  * gethash.js
  * This Script Created by Onur KOL
- * version: 1.0.0
+ * version: 1.0.5
  *
  * Example :
  *   Location : .../index.html#text:Hello World
@@ -10,23 +10,33 @@
  * Note!
  * First include this file.
 */
-var hash = window.location.hash.substring(1);
-var hc=hash.split(":");
-var hashcode=hc[0];
-var hashval="";
-var hvlimit=hc.length;
-for(var a=1; a<hvlimit; a++){
-	hashval+=hc[a];
+var strlist=new Array();
+var vallist=new Array();
+var hash = window.location.hash.substring(0);
+var checkhash=hash.split("#");
+var checkn=checkhash.length;
+
+//listing command list: '#STR:VALUE#STR:VALUE#STR:VALUE';
+// and processing separation string and values.
+for(var a=1; a<checkn; a++){
+	var checkt=checkhash[a].split(":");
+	var checkt_str=checkt[0];
+	var checkt_val=checkt[1];
+	
+	strlist[a]=checkt_str;
+	vallist[a]=checkt_val;
 }
 
 function get(v){
-	if(v==hashcode){
-		return hashval;
-	}
-	else{
-		return "";
+	var f=strlist.length;
+
+	for(var b=1; b<f; b++){
+		if(v==strlist[b]){
+			return vallist[b];
+		}
 	}
 }
+
 
 window.onhashchange=function(){ 
 	window.location.reload(false);
